4813fd46ce48c847104aa8279e64bc7b081f2801
Victor Orlunda <vicorlunda@gmail.com>
2021-05-07 14:47:47 +0100

    add test that ensures correct encryption irrespective of case

--- test/caeser.test.js
+++ test/caeser.test.js
@@ -3,3 +3,7 @@ const caeser = require('../src/caeser');
 test('should encrypt a message correctly', () => {
   expect(caeser('attack at dawn', 2)).toBe('cvvcem cv fcyp');
 });
+
+test('encrypted messages should keep the same message as plainText', () => {
+  expect(caeser('Attack at daWn', 2)).toBe('Cvvcem cv fcYp');
+});
