b0fc5b11149f56192c411109c91b0736dd32759a
Victor Orlunda <vicorlunda@gmail.com>
2021-05-08 16:14:45 +0100

    add test suite to check that expected average value is close to returned value

--- test/array.test.js
+++ test/array.test.js
@@ -10,3 +10,8 @@ test('returned obj should have average, min, max, and length properties', () => 
   expect(analyze([1, 2, 3, 4, 5]).hasOwnProperty('min')).toBeTruthy();
   expect(analyze([1, 2, 3, 4, 5]).hasOwnProperty('length')).toBeTruthy();
 });
+
+test('returned obj.average should give the true average of the array of numbers', () => {
+  expect(analyze([1, 2, 3, 4, 5]).average).toBeCloseTo(3);
+  expect(analyze([1, 2, 3, 4]).average).toBeCloseTo(2.5);
+});
