b21f91f75b18a2597f4db0585d22a6de7a3c628b
Victor Orlunda <vicorlunda@gmail.com>
2021-05-08 15:46:44 +0100

    add test for encrypting messages with large keys

--- test/caeser.test.js
+++ test/caeser.test.js
@@ -11,3 +11,8 @@ test('encrypted messages should keep the same message as plainText', () => {
 test('encrypted messages should keep their punctuation', () => {
   expect(caeser('attack, at ,dawn', 2)).toBe('cvvcem, cv ,fcyp');
 });
+
+test('should encrypt messages correctly for numbers between 0-25', () => {
+  expect(caeser('attack at dawn', 9)).toBe('jccjlt jc mjfw');
+  expect(caeser('attack at dawn', 25)).toBe('zsszbj zs czvm');
+});
