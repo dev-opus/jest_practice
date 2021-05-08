e06776cf2708580de1ef6bcb24797e78f0040ea2
Victor Orlunda <vicorlunda@gmail.com>
2021-05-08 16:06:23 +0100

    add test suite to check that returned obj consits of required props

--- test/array.test.js
+++ test/array.test.js
@@ -3,3 +3,10 @@ const analyze = require('../src/arrayAnalyze');
 test('should return an object with 4 four props', () => {
   expect(Object.keys(analyze([1, 2, 3, 4, 5])).length).toBe(4);
 });
+
+test('returned obj should have average, min, max, and length properties', () => {
+  expect(analyze([1, 2, 3, 4, 5]).hasOwnProperty('average')).toBeTruthy();
+  expect(analyze([1, 2, 3, 4, 5]).hasOwnProperty('max')).toBeTruthy();
+  expect(analyze([1, 2, 3, 4, 5]).hasOwnProperty('min')).toBeTruthy();
+  expect(analyze([1, 2, 3, 4, 5]).hasOwnProperty('length')).toBeTruthy();
+});
